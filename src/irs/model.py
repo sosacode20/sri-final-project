ec83c15d49d263e1ee4b29d3d8153d5fb73a96ab
IronBeardX <alejandroyeroval@gmail.com>
2022-11-13 23:51:56 -0500

    creation of model class

---
+++ src/irs/model.py
@@ -0,0 +1,15 @@
+class Models:
+    def __init__(self):
+        self.__indexing_documents = []
+
+    def indexing_documents(self, documents):
+        pass
+
+    def index_document(self, document):
+        pass
+
+    def index_query(self, query):
+        pass
+
+    def get_sim(self, query):
+        pass
