5909b70c6c1247a4937d722ffd2f82988929ca61
IronBeardX <alejandroyeroval@gmail.com>
2022-11-16 01:02:04 -0500

    adding time measurement

--- cli.py
+++ cli.py
@@ -3,10 +3,19 @@ from src.models import vector_space_model
 from src.storage import Storage
 from src import utils
 from src.parser import CranParser
+import time
+
+def time_convert(sec):
+  mins = sec // 60
+  sec = sec % 60
+  hours = mins // 60
+  mins = mins % 60
+  print("Time Lapsed = {0}:{1}:{2}".format(int(hours),int(mins),sec))
 
 def main():
     print(chr(27) + "[2J")
     print("Cargando la colección Cranfield de documentos...")
+    start_time = time.time()
 
     storage = Storage()
     vector_model = vector_space_model.Vector_Model(utils.processing_text)
@@ -24,6 +33,8 @@ def main():
         ranking = vector_model.get_ranking(query, 5)
         for doc in ranking:
             print(f'DOC_ID:{doc.doc_id}\nDOC_NAME:{doc.doc_name}\n')
+        end_time = time.time()
+        print(time_convert(end_time - start_time))
         print("Introduzca 'q' para terminar la ejecución o cualquier otra tecla para realizar otra consulta:")
         if (input() == 'q'): break
 
