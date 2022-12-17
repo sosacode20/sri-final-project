bf4c5c99576e4ed3f8139157fccfbea8fecac458
IronBeardX <alejandroyeroval@gmail.com>
2022-12-17 03:25:21 -0500

    changing the api having front in mind

--- main.py
+++ main.py
@@ -22,8 +22,10 @@ app = FastAPI()
 #Initializing the application
 storage = Storage()
 irs_instance = IRS(storage)
+selected_model = "None"
 
 '''
+
 Application Workflow:
 [x] User defines data collection // TODO: If theres enough time, implement 
 a way to receive the data collection directory
@@ -33,8 +35,10 @@ a way to receive the data collection directory
 cache then it should be loaded from there (ask for cache format)
 
 New Functionalities with front in mind
-[ ] Getting instantiated models
-[ ] 
+[x] Getting instantiated Models
+[ ] Getting collections from Models
+[ ] Queries are only done in a model and collection specific
+[ ] Getting ranked documents with offset
 
 '''
 
@@ -51,10 +55,9 @@ class CollectionName(str, Enum):
 class Document(BaseModel):
     id: int
     title: str
-
-class DocumentWithBody(Document):
     body: str
 
+'''
 #TODO: Divide this in two different endpoints
 @app.get("/api")
 def set_model_collection(
@@ -64,8 +67,7 @@ def set_model_collection(
     #TODO: Remove this when its done
     model = None
     if model_name == ModelName.placeholder or collection == CollectionName.placeholder:
-        return {"Error": "Model or collection not implemented yet"}
-    
+        return {"Error": "Model or collection not implemented yet"}  
     elif model_name == ModelName.prob:
         model = probabilistic_model.Probabilistic_Model(utils.processing_text)
     elif model_name == ModelName.vect:
@@ -87,26 +89,8 @@ def set_model_collection(
     irs_instance.add_document_collection("./data", parser.get_pretty_name(), model.get_name())
     return {"Message": "IRS initialized"}
 
-@app.get("/api/query", response_model=list[Document | DocumentWithBody])
-def query(
-    query: str = Query(default=..., title="Query", description="A query to be used by the retrieval system"),
-    limit: int = Query(default=10, title="Limit", description="The number of documents from the ranking to be returned"),
-    model_name: ModelName = Query(default=..., title="Model", description="The model to be used for processing the query")
-    ):
-    query.strip()
-    if query == '':
-        return {"Error": "Query is empty"}
-
-    documents = []
-    #TODO: I must limit the models clients can send so they can't send a model that is not in the IRS
-    ranking = irs_instance.get_ranking(query, model_name, limit)
-    for doc in ranking:
-        documents.append(Document(id=doc[0], title=doc[1]))
-    
-    return documents
-
 #TODO: This does not work yet
-@app.get("/api/document/{model_name}/{id}", response_model=DocumentWithBody)
+@app.get("/api/document/{model_name}/{id}", response_model=Document)
 def get_document(
     id: int = Path(..., 
      title="Document ID", description="The ID of the document to be returned"),
@@ -114,7 +98,86 @@ def get_document(
      title="Model", description="The model to be used for processing the query")
     ):
     document = irs_instance.models[model_name].get_document_by_id(id)
-    return DocumentWithBody(id=document.doc_id, title=document.doc_id, body=document.doc_body)
+    return Document(id=document.doc_id, title=document.doc_id, body=document.doc_body)
+
+#####################################
+#    New Back With Front in Mind    #
+#####################################
+'''
+
+
+@app.get("/api/model_options", response_model = list[str])
+def get_model_options():
+    return ["Vectorial Space Model", "Probabilistic Model"]
+
+@app.get("/api/collection_options", response_model = list[str])
+def get_collection_options():
+    return ["Cran", "Reuters"]
+
+@app.get("/api/instantiated_models", response_model = list[str])
+def get_instantiated_models():
+    return irs_instance.list_models() 
+
+@app.get("/api/selected_model", response_model = str)
+def get_selected_model():
+    return selected_model
+
+#TODO: Return rating
+@app.get("/api/query", response_model = list[Document])
+def make_query(
+    query: str = Query(default=..., title="Query", description="A query to be used by the retrieval system", regex="^(?!\s*$).+"),
+    limit: int = Query(default=10, title="Limit", description="The number of documents from the ranking to be returned"),
+    offset: int = Query(default=0, title="offset", description="The number of documents from the ranking to be skipped"),
+    summary_len: int = Query(default=-1, title="Summary Length", description="The number of characters from the document body to be returned")
+):
+    query.strip()
+    documents = []
+    ranking = irs_instance.get_ranking(query, selected_model, limit)#TODO: add offset
+    return [Document(id=doc[0], title=doc[1], body=doc[2][: None if summary_len == -1 else summary_len]) for doc in ranking]
+
+    for doc in ranking:
+        documents.append(Document(id=doc[0], title=doc[1], body=doc[3][: None if summary_len == -1 else summary_len]))
+    return documents
+
+@app.post("/api/select_model/{model_name}", response_model = str)
+def select_model(
+     model_name: ModelName = Path(default=..., title="Model", description="The model to be used for processing the query"),
+     collection_list: list[CollectionName] = Body(default=..., title="Collection List", description="The list of collections to be used for processing the query")
+    ):
+    #TODO: What happens if i add new documents to an already instantiated model?
+    model = None
+    if model_name == ModelName.prob and model_name not in irs_instance.list_models():
+        model = probabilistic_model.Probabilistic_Model(utils.processing_text)
+    elif model_name == ModelName.vect and model_name not in irs_instance.list_models():
+        model = vector_space_model.Vector_Model(utils.processing_text)
+    else:
+        return {"Error": "Invalid model selected"}
+
+    collection_parsers = []
+    for collection in collection_list:
+        if collection == CollectionName.cran and collection not in irs_instance.list_parsers():
+            collection_parsers.append(CranParser(utils.processing_text))
+        elif collection == CollectionName.reuter and collection not in irs_instance.list_parsers():
+            collection_parsers.append(ReutersParser(utils.processing_text))
+        else:
+            return {"Error": "Invalid collection selected"}
+    
+    irs_instance.add_model(model)
+    for parser in collection_parsers:
+        irs_instance.add_parser(parser)
+        irs_instance.add_document_collection("./data", parser.get_pretty_name(), model.get_name())
+    
+    global selected_model 
+    selected_model = model.get_name()
+    return "Model instantiated correctly"
+
+@app.get("/api/clear", status_code=204)
+def clear():
+    global irs_instance
+    irs_instance = IRS(Storage())
+    global selected_model
+    selected_model = None
+    return {}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
