b53e561f34b63261995bb70c4008897107a58711
IronBeardX <alejandroyeroval@gmail.com>
2022-12-20 02:09:13 -0500

    probabilistic model fixed with ps-feedback

--- src/models/probabilistic_model.py
+++ src/models/probabilistic_model.py
@@ -9,132 +9,109 @@ class Probabilistic_Model(Model):
 
     def __init__(self, text_processor: Callable[[str, str], list[str]]):
         super().__init__(text_processor)
-
-        self.document_vectors: list[list[str]] = []
-        """
-        This is the dictionary where the key is the document_id and the value is a list of tuples (term_index, occurrence)
-        """
-        # This acts as a cache for storing the last ranking of a consult, this is in the case of handling result pages
-        self.last_ranking: list[tuple[float, int]] = []
-        
-        self.query_document_relevance: dict[tuple[str, int], dict[str, float]] = {}
-
-        self.query_document_not_relevance: dict[tuple[str, int], dict[str, float]] = {}
-        """
-        This contains the relevance of a query in a document
-        """
+        self.inverted_index = {}
+        self.weights = {}
+        self.ranked = []
+        self.dirty = 0
 
     def get_name(self):
         return "Probabilistic_Model"
 
+    def document_frequency(self, term: str) -> int:
+        return len(self.inverted_index[term])
+
+    def inverse_document_frequency(self, term: str, df: int) -> float:
+        aux = len(self.documents) / df
+
+        return math.log(aux)
+    #[ ]
     def add_document(self, document: Document):
+        self.dirty = 1
         self.documents.append(document)
-        self.document_vectors.append([])
-        for term in (document.doc_normalized_name + document.doc_normalized_body):
-            self.document_vectors[-1].append(term)
 
-    def generate_document_vectors(self):
-        """Generate the document vectors for the model
-        Returns:
-            None: The documents vector are calculated here and stored in the model for later use
-        """
-        self.document_vectors = []
+    def generate_inverted_index(self):
+        self.inverted_index = {}
+        for i in range(len(self.documents)):
+            for term in self.documents[i].doc_normalized_name + self.documents[i].doc_normalized_body:
+                if term not in self.inverted_index:
+                    self.inverted_index[term] = set()
+                self.inverted_index[term].add(i)
 
-        for doc_index, doc in enumerate(self.documents):
-            self.document_vectors.append([])
-            for term in (doc.doc_normalized_name + doc.doc_normalized_body):
-                self.document_vectors[doc_index].append(term)
+    def rsv_weight(self) -> None:
+        N = len(self.documents)
+        self.weights = { }
+        for term in self.inverted_index:
+            p = self.document_frequency(term) / (N + 0.5)
+            t1 = self.inverse_document_frequency(term, p)
+            t2 = p/(1-p)
+            self.weights[term] = t1 + math.log(t2)
 
-    def get_term_frequency(self, term: str) -> float:
-        n_i = 0
-        for doc in (self.document_vectors):
-            if term in doc:
-                n_i += 1
-        return n_i / len(self.documents)
+    #[ ]
+    def rsv_doc_query(self, query:str, doc_id:int, query_vector:list[str]):
+        score = 0
+        if doc_id >= len(self.documents):
+            pass
+        doc = self.documents[doc_id]
+
+        for term in query_vector:
+            if term in doc.doc_normalized_name + doc.doc_normalized_body:
+                score += self.weights[term]
+        return score
+
+    #[ ]
+    def _get_ranking(self, query:str, query_vector: list[str]):
+        docs = set()
+        for term in query_vector:
+            if term in self.inverted_index:
+                docs_with_term = self.inverted_index[term]
+                for docs_id in docs_with_term:
+                    docs.add(docs_id)
+
+        scores = []
+        for doc in docs:
+            scores.append((doc, self.rsv_doc_query(query, doc, query_vector)))
+
+        self.ranked = sorted(scores, key=lambda x: x[1], reverse=True)
+        return self.ranked
+
+    def get_ranking(self, query:str, amount:int, offset:int, lang:str = 'english') -> list[tuple[Document, int]]:
+        if self.dirty:
+            self.generate_inverted_index()
+            self.rsv_weight()
+            self.dirty = 0
+        query_vector = self.generate_query_vector(query, lang)
+        ranking = self._get_ranking(query, query_vector)
+
+        ## pseudo-feedback
+        i = 0
+        new_ranking = []
+        while (i < 10) and ranking != new_ranking:
+            self.recompute_weights(query_vector)
+            new_ranking = self._get_ranking(query, query_vector)
+            i += 1
+        ranking = new_ranking
+
+        return [(self.documents[doc], rank) for doc, rank in ranking[offset: offset + amount]]
 
     def generate_query_vector(self, query: set, lang: str = 'english'):
         return self.text_processor(query, lang)
-
-    def get_relevance(self, query, document_id, term) -> float:
-        if (query, document_id) in self.query_document_relevance:
-            if term in self.query_document_relevance[(query, document_id)]:
-                dr = self.query_document_relevance[(query, document_id)][term]
-                dnr = self.query_document_not_relevance[(query, document_id)][term]
-                return (dr, dnr) 
-
-            dr = 0.5
-            dnr = self.get_term_frequency(term)
-            self.query_document_relevance[(query, document_id)][term] = dr
-            self.query_document_not_relevance[(query, document_id)][term] = dnr
-            return (dr, dnr)
-
-        dr = 0.5
-        dnr = self.get_term_frequency(term)
-
-        self.query_document_relevance[(query, document_id)] = { term:dr }
-        self.query_document_not_relevance[(query, document_id)] = { term:dnr }
-
-        return (dr, dnr)
-
-    def similarity(self, raw_query:str, query: list[str], document: list[str], document_id: int) -> float:
-        """Calculate the similarity between a query and a document"""
-        similarity = 0
-
-        for common_term in query:
-            if common_term in document:
-                p_i, r_i = self.get_relevance(raw_query, document_id, common_term)
-                similarity += math.log((p_i * (1 - r_i)) / (r_i * (1 - p_i)))
-
-        return (0 if similarity == 0 else math.log(similarity))
-            
-    def get_ranking(self, query: str, first_n_results: int, offset:int, lang: str = 'english') -> list[tuple[Document, int]]:
-        query_vector = self.generate_query_vector(query, lang)
-        doc_rank: list[tuple[float, int]] = []
-        for index, _ in enumerate(self.documents):
-            doc_vector = self.document_vectors[index]
-            sim = self.similarity(query, query_vector, doc_vector, index)
-            doc_rank.append((sim, index))
-        self.last_ranking = sorted(
-            doc_rank, key=lambda rank_index: rank_index[0], reverse=True)
+ 
+    #[ ]
+    def recompute_weights(self, query_vector: list[str]):
+        relevant_docs = []
+        for idx in range(5):
+            doc_id = self.ranked[idx][0]
+            relevant_docs.append(self.documents[doc_id])
         
-        #the commented one only returns ranking != 0
-        # return [] if self.lastranking[0][0] == 0 else [(self.documents[doc], rank) for rank, doc in self.last_ranking[offset: offset + first_n_results]]
-        return [(self.documents[doc], rank) for rank, doc in self.last_ranking[offset: offset + first_n_results]]
-
-    def total_relevant_documents(self) -> int:
-        total = 0
-        i = 0
-        while(self.last_ranking[i][0] != 0):
-            total += 1
-            i += 1
-        return total
-
-    def total_ocurrences(self, term: str) -> int:
-        total = 0
-        for doc in self.document_vectors:
-            if term in doc:
-                total += 1
-        return total
-
-    #[ ]: limit tiene que ser menot que relevant_documents_cant
-    def pseudo_feedback(self, raw_query:str, lang:str = 'english', limit:int = 10):
-        query_vector = self.generate_query_vector(raw_query, lang)
-        V = self.total_relevant_documents()
         N = len(self.documents)
+        ni = len(relevant_docs)
 
-        ### Calculating term ocurrences in relevant documents
         for term in query_vector:
-            ni = self.total_ocurrences(term)
-            Vi = 0
-            doc_index_list = []
-            for i in range(limit):
-                rank, doc_index = self.last_ranking[i]
-                if rank == 0:
-                    break
-                if term in self.document_vectors[doc_index]:
-                    Vi += 1
-                    doc_index_list.append(doc_index)
-            for doc_index in doc_index_list:
-                self.query_document_relevance[(raw_query, doc_index)][term] = (Vi / V)
-                self.query_document_not_relevance[(raw_query, doc_index)][term] = (ni - Vi) / (N - V)
-                
+            if term in self.weights.keys():
+                Vi = 0
+                for doc in relevant_docs:
+                    if term in doc.doc_normalized_name + doc.doc_normalized_body:
+                        Vi += 1
+                p = (Vi + 0.5) / (ni + 1)
+                u = (self.document_frequency(term) - Vi + 0.5) / (N - ni + 1)
+                self.weights[term] = math.log((1-u)/u) + math.log(p/(1-p))
